0e3bec794493cfe68fe5aa3ccc6818a0b847ca2f
leeseungmin <lsm0341@naver.com>
2020-07-29 16:18:11 +0900

    2020-07-29

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class DemoController {
+
+    Code code;
+
+    @GetMapping("/")
+    public String main(){
+        return "index";
+    }
+
+    @PostMapping("/code")
+    public String setCode(String code){
+        this.code.setCode(code);
+        System.out.println(this.code.getCode());
+        return "result";
+    }
+
+
+}
+
